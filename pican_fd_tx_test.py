6738aa36fd407d802305eba234ce8c8d69a15c2d
jotempel <johannes.tempel@viconnis.de>
2020-09-15 15:15:27 +0200

    Update pican_fd_tx_test.py
    removed gpio import and usage

--- pican_fd_tx_test.py
+++ pican_fd_tx_test.py
@@ -6,16 +6,11 @@
 # http://skpang.co.uk/catalog/canbus-fd-board-with-real-time-clock-for-raspberry-pi-3-p-1545.html
 #
 
-import RPi.GPIO as GPIO
 import os
 import can
 import time
 led = 22
 count = 0
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(led,GPIO.OUT)
-GPIO.output(led,True)
 
 print('\n\rCAN Tx test')
 print('Bring up CAN0....')
@@ -28,7 +23,6 @@ try:
 	bus = can.interface.Bus(channel='can0', bustype='socketcan_native',fd = True)
 except OSError:
 	print('Cannot find PiCAN FD board.')
-	GPIO.output(led,False)
 	exit()
 	
 
@@ -40,4 +34,3 @@ for m in range(0,16):
      time.sleep(0.02)
 time.sleep(0.05)
 os.system("sudo /sbin/ip link set can0 down")
-GPIO.output(led,False)
